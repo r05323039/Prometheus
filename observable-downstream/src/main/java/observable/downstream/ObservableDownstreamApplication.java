8cb5be56fac903d676530129f905c2406ea5240a
ian.lin@webcomm.com.tw <ian.lin@webcomm.com.tw>
2025-12-20 12:44:08 +0800

    pom結構

---
+++ observable-downstream/src/main/java/observable/downstream/ObservableDownstreamApplication.java
@@ -0,0 +1,13 @@
+package observable.downstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservableDownstreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObservableDownstreamApplication.class, args);
+    }
+
+}
---
+++ observable-service/src/main/java/observable/service/ObservableServiceApplication.java
@@ -0,0 +1,13 @@
+package observable.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservableServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObservableServiceApplication.class, args);
+    }
+
+}
