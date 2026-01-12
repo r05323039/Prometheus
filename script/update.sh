407c723747363cccfe5c2d0ee39938a72434fcc3
ian.lin <ian.lin@webcomm.com.tw>
2026-01-12 14:45:47 +0800

    fix update.sh

--- script/update.sh
+++ script/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -v -X POST http://localhost:9090/-/reload
-curl -v -X POST http://localhost:9093/-/reload
+curl -v -X POST http://localhost/prometheus/-/reload
+curl -v -X POST http://localhost/alertmanager/-/reload
